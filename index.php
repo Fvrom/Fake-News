c034262f9eaa69400d889706c0a95c7871bc74be
root <root@Ida-Dator.localdomain>
2020-10-19 13:06:46 +0200

    basic html and css added

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="header">
+        <h2>Blog Name</h2>
+    </div>
+
+    <div class="wrapper">
+
+        <div class="column">
+            <div class="card">
+                <h2>TITLE HEADING</h2>
+
+                <div class="fakeimg" style="height:200px;">Image</div>
+
+                <p>Some text..</p>
+                <h5>Author, Title description, Dec 7, 2017</h5>
+            </div>
+
+
+            <div class="card">
+                <h2>TITLE HEADING</h2>
+
+                <div class="fakeimg" style="height:200px;">Image</div>
+                <p>Some text..</p>
+                <h5>Author, Title description, Sep 2, 2017</h5>
+            </div>
+        </div>
+
+
+
+    </div>
+
+    <div class="footer">
+        <h2>Footer</h2>
+    </div>
+
+</body>
+
+</html>
