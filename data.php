cfa76249e0594d9db84ed107ed0aa6eeb8a15688
root <root@Ida-Dator.localdomain>
2020-10-26 18:32:26 +0100

    made changes to mobile version. Added subtext to data.

--- data.php
+++ data.php
@@ -17,46 +17,52 @@ $authors = [
 
 $articles =  [
     [
-        'title' => 'Heading1',
+        'title' => 'Hogwarts student drank mysterious potion',
         'content' => 'Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house dementors betrayal. Holly, Snape centaur portkey ghost Hermione spell bezoar Scabbers. 
         Peruvian-Night-Powder werewolf, Dobby pear-tickle half-moon-glasses, Knight-Bus. Padfoot snargaluff seeker: Hagrid broomstick mischief managed. 
         Snitch Fluffy rock-cake, 9 ¾ dress robes I must not tell lies. Mudbloods yew pumpkin juice phials Ravenclaw’s Diadem 10 galleons Thieves Downfall. 
         Ministry-of-Magic mimubulus mimbletonia Pigwidgeon knut phoenix feather other minister Azkaban. Hedwig Daily Prophet treacle tart full-moon Ollivanders You-Know-Who cursed. 
         Fawkes maze raw-steak Voldemort Goblin Wars snitch Forbidden forest grindylows wool socks.',
         'author' => $authors[4]['name'],
+        'subtext' => 'Hogwarts student drank mysterious potion, got tall as a tree and ran out into the woods.',
         'published_date' => '2020-02-10',  // Kanske lägga datum i en ny array?
         'likes' => '15',
-        'image' => '/img/image1.jpg'
+        'image' => '/img/image1.jpg',
+        'id' => '1'
     ],
 
     [
-        'title' => 'Heading2',
+        'title' => 'Blackmarket in Diagon Alley',
         'content' => "Thestral dirigible plums, Viktor Krum hexed memory charm Animagus Invisibility Cloak three-headed Dog. 
         Half-Blood Prince Invisibility Cloak cauldron cakes, hiya Harry! Basilisk venom Umbridge swiveling blue eye Levicorpus, 
         nitwit blubber oddment tweak. Chasers Winky quills The Boy Who Lived bat spleens cupboard under the stairs flying motorcycle. 
         Sirius Black Holyhead Harpies, you’ve got dirt on your nose. Floating candles Sir Cadogan The Sight three hoops disciplinary hearing. 
         Grindlewald pig’s tail Sorcerer's Stone biting teacup. Side-along dragon-scale suits Filch 20 points, Mr. Potter.",
         'author' => $authors[3]['name'],
+        'subtext' => 'Ministry busts more shops in Diagon Alley. Dragon eggs a hot topic',
         'published_date' => '2020-02-12',  // Kanske lägga datum i en ny array?
         'likes' => '10',
-        'image' => '/img/image2.jpg'
+        'image' => '/img/image2.jpg',
+        'id' => '2'
     ],
 
     [
-        'title' => 'Heading3',
+        'title' => 'Harry Potter and Hogwarts',
         'content' => "Half-giant jinxes peg-leg gillywater broken glasses large black dog Great Hall. 
         Nearly-Headless Nick now string them together, and answer me this, which creature would you be unwilling to kiss? 
         Poltergeist sticking charm, troll umbrella stand flying cars golden locket Lily Potter. Pumpkin juice Trevor wave your wand out glass orbs, 
         a Grim knitted hats. Stan Shunpike doe patronus, suck his soul Muggle-Born large order of drills the trace. Bred in captivity fell through the veil, 
         quaffle blue flame ickle diddykins Aragog. Yer a wizard, Harry Doxycide the woes of Mrs. Weasley Goblet of Fire. ",
         'author' => $authors[2]['name'],
+        'subtext' => 'Potter talks about getting the letter from Hogwarts.',
         'published_date' => '2020-02-08',  // Kanske lägga datum i en ny array?
         'likes' => '30',
-        'image' => '/img/image3.jpg'
+        'image' => '/img/image3.jpg',
+        'id' => '3'
     ],
 
     [
-        'title' => 'Heading4',
+        'title' => 'Is Slytherin all bad?',
         'content' => "Red hair crookshanks bludger Marauder’s Map Prongs sunshine daisies butter mellow Ludo Bagman. Beaters gobbledegook N.E.W.T., 
         Honeydukes eriseD inferi Wormtail. Mistletoe dungeons Parseltongue Eeylops Owl Emporium expecto patronum floo powder duel. Gillyweed portkey, 
         keeper Godric’s Hollow telescope, splinched fire-whisky silver Leprechaun O.W.L. stroke the spine. Chalice Hungarian Horntail, catherine wheels Essence of Dittany Gringotts 
@@ -69,70 +75,82 @@ $articles =  [
          knut phoenix feather other minister Azkaban. Hedwig Daily Prophet treacle tart full-moon Ollivanders You-Know-Who cursed. Fawkes maze raw-steak Voldemort Goblin Wars snitch 
          Forbidden forest grindylows wool socks.",
         'author' => $authors[1]['name'],
+        'subtext' => 'The ranks of evil wizards seem to come from Slytherin.. but is that really all true?',
         'published_date' => '2020-02-07',  // Kanske lägga datum i en ny array?
         'likes' => '24',
-        'image' => '/img/image4.jpg'
+        'image' => '/img/image4.jpg',
+        'id' => '4'
     ],
 
     [
-        'title' => 'Heading5',
+        'title' => 'History of the houses',
         'content' => "Prefect’s bathroom Trelawney veela squashy armchairs, SPEW: Gamp’s Elemental Law of Transfiguration. Magic Nagini bezoar, Hippogriffs Headless Hunt giant squid petrified.
          Beuxbatons flying half-blood revision schedule, Great Hall aurors Minerva McGonagall Polyjuice Potion. Restricted section the Burrow Wronski Feint gnomes, 
          quidditch robes detention, chocolate frogs. Errol parchment knickerbocker glory Avada Kedavra Shell Cottage beaded bag portrait vulture-hat. Twin cores, Aragog crimson gargoyles,
           Room of Requirement counter-clockwise Shrieking Shack. Snivellus second floor bathrooms vanishing cabinet Wizard Chess, are you a witch or not?",
         'author' => $authors[0]['name'],
+        'subtext' => 'Album Dumbledore gives you a fun read about the different houses in Hogwarts.',
         'published_date' => '2020-02-06',  // Kanske lägga datum i en ny array?
         'likes' => '14',
-        'image' => '/img/image5.jpg'
+        'image' => '/img/image5.jpg',
+        'id' => '5'
     ],
 
     [
-        'title' => 'Heading6',
+        'title' => 'Dragon seen in Diagon Alley!',
         'content' => "Squashy armchairs dirt on your nose brass scales crush the Sopophorous bean with flat side of silver dagger, releases juice better than cutting. 
         Full moon Whomping Willow three turns should do it lemon drops. Locomotor trunks owl treats that will be 50 points, Mr. Potter. Witch Weekly, 
         he will rise again and he will come for us, headmaster Erumpent horn. Fenrir Grayback horseless carriages ‘zis is a chance many would die for!",
         'author' => $authors[0]['name'],
+        'subtext' => ' An unregistred Dragon escaped from owner and flew over Diagon Alley this afternoon. Ministry busts yet another... ',
         'published_date' => '2020-02-05',  // Kanske lägga datum i en ny array?
         'likes' => '16',
-        'image' => '/img/image6.jpg'
+        'image' => '/img/image6.jpg',
+        'id' => '6'
     ],
 
     [
-        'title' => 'Heading7',
+        'title' => 'The express an old transport?',
         'content' => "Boggarts lavender robes, Hermione Granger Fantastic Beasts and Where to Find Them. Bee in your bonnet Hand of Glory elder wand, spectacles House Cup Bertie Bott’s 
         Every Flavor Beans Impedimenta. Stunning spells tap-dancing spider Slytherin’s Heir mewing kittens Remus Lupin.
          Palominos scarlet train black robes, Metamorphimagus Niffler dead easy second bedroom. Padma and Parvati Sorting Hat Minister of Magic blue turban remember my last.",
         'author' => $authors[4]['name'],
+        'subtext' => 'Is the express really the best transport? We have talked to several wizards.',
         'published_date' => '2020-02-04',  // Kanske lägga datum i en ny array?
         'likes' => '29',
-        'image' => '/img/image7.jpg'
+        'image' => '/img/image7.jpg',
+        'id' => '7'
     ],
 
     [
-        'title' => 'Heading8',
+        'title' => 'Dragons! and more dragons! ',
         'content' => "Thestral dirigible plums, Viktor Krum hexed memory charm Animagus Invisibility Cloak three-headed Dog. Half-Blood Prince Invisibility Cloak cauldron cakes, 
         hiya Harry! Basilisk venom Umbridge swiveling blue eye Levicorpus, nitwit blubber oddment tweak. Chasers Winky quills The Boy Who Lived bat spleens cupboard under the stairs flying motorcycle. 
         Sirius Black Holyhead Harpies, you’ve got dirt on your nose. Floating candles Sir Cadogan The Sight three hoops disciplinary hearing. 
         Grindlewald pig’s tail Sorcerer's Stone biting teacup. Side-along dragon-scale suits Filch 20 points, Mr. Potter.        ",
         'author' => $authors[1]['name'],
+        'subtext' => 'Dragon! dragon! more dragons seen in Diagon Alley. Ministry fighting the blackmarket.',
         'published_date' => '2020-02-03',  // Kanske lägga datum i en ny array?
         'likes' => '34',
-        'image' => '/img/image8.jpg'
+        'image' => '/img/image8.jpg',
+        'id' => '8'
     ],
 
     [
-        'title' => 'Heading9',
+        'title' => 'Best school for magic?',
         'content' => "Toad-like smile Flourish and Blotts he knew I’d come back Quidditch World Cup. Fat Lady baubles banana fritters fairy lights Petrificus Totalus. So thirsty, 
         deluminator firs’ years follow me 12 inches of parchment. Head Boy start-of-term banquet Cleansweep Seven roaring lion hat. Unicorn blood crossbow mars is bright tonight, 
         feast Norwegian Ridgeback. Come seek us where our voices sound, we cannot sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.",
         'author' => $authors[2]['name'],
+        'subtext' => 'Is hogwarts still the best school for your kids to learn about magic? Find out!',
         'published_date' => '2020-02-02',  // Kanske lägga datum i en ny array?
         'likes' => '21',
-        'image' => '/img/image9.jpg'
+        'image' => '/img/image9.jpg',
+        'id' => '9'
     ],
 
     [
-        'title' => 'Heading10',
+        'title' => 'Mandrakes found useful in medicine! ',
         'content' => "Squashy armchairs dirt on your nose brass scales crush the Sopophorous bean with flat side of silver dagger, releases juice better than cutting. 
         Full moon Whomping Willow three turns should do it lemon drops. Locomotor trunks owl treats that will be 50 points, Mr. Potter. Witch Weekly, he will rise again and he will come for us, 
         headmaster Erumpent horn. Fenrir Grayback horseless carriages ‘zis is a chance many would die for!
@@ -140,9 +158,11 @@ $articles =  [
         Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house dementors betrayal. Holly, Snape centaur portkey ghost Hermione spell bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby pear-tickle half-moon-glasses, Knight-Bus. Padfoot snargaluff seeker: Hagrid broomstick mischief managed. Snitch Fluffy rock-cake, 9 ¾ dress robes I must not tell lies. Mudbloods yew pumpkin juice phials Ravenclaw’s Diadem 10 galleons Thieves Downfall. Ministry-of-Magic mimubulus mimbletonia Pigwidgeon knut phoenix feather other minister Azkaban. Hedwig Daily Prophet treacle tart full-moon Ollivanders You-Know-Who cursed. 
         Fawkes maze raw-steak Voldemort Goblin Wars snitch Forbidden forest grindylows wool socks.",
         'author' => $authors[3]['name'],
+        'subtext' => 'Medicine using Mandrakes have proven to be a lot more sufficent than orginially thought, shown with new experiments. ',
         'published_date' => '2020-02-01',  // Kanske lägga datum i en ny array?
         'likes' => '17',
-        'image' => '/img/image10.jpg'
+        'image' => '/img/image10.jpg',
+        'id' => '10'
     ],
 
 
--- index2.php
+++ index2.php
@@ -24,43 +24,62 @@ require __DIR__ . '/data.php'; ?>
 <body>
     <header>
         <h1> Fake Prophet </h1>
+
+        <button class="hamburger">
+            <!-- burger menu taken from elinas lesson  -->
+            <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
+                <g clip-path="url(#clip0)" fill="#fff">
+                    <path d="M20 40a20 20 0 1120-20 20.022 20.022 0 01-20 20zm0-38.75A18.75 18.75 0 1038.75 20 18.772 18.772 0 0020 1.25z" />
+                    <path d="M29.688 13.75H10.313a.625.625 0 110-1.25h19.374a.624.624 0 110 1.25zM29.688 20.625H10.313a.625.625 0 110-1.25h19.374a.624.624 0 110 1.25zM29.688 27.5H10.313a.625.625 0 110-1.25h19.374a.624.624 0 110 1.25z" />
+                </g>
+                <defs>
+                    <clipPath id="clip0">
+                        <path fill="#fff" d="M0 0h40v40H0z" />
+                    </clipPath>
+                </defs>
+            </svg>
+        </button>
+
     </header>
 
-    <section>
+    <main>
+        <section>
 
-
-        <div class="wrapper">
-            <!-- <div clasS="sidebar">
-                <div class="ads">
-
-                    <?php // $ad = getRandomAd($commercial); 
-                    ?>
-                    <h3>
-                        <?php // echo $ad['title']; 
-                        ?> </h3>
-                    <p> <?php // echo $ad['content'];
-                        ?> </p>
-
-
-                </div>
-            </div> -->
 
             <div class="main-content">
 
                 <?php $articles = sortPostByPublish($articles); // sorting your articles with the function
-                foreach ($articles as $article) : ?>
+                foreach ($articles as $article) :
+                    $id = $article['id'] ?>
+                    <!-- get id for every article -->
+
                     <article class="article_content">
-                        <h2>
+
+                        <h2 class="title_content">
                             <?= $article['title']; ?>
                         </h2>
-                        <p class="content">
-                            <?= $article['content']; ?>
-                        </p>
+
+
                         <div class="article_image"> <img src=" <?= $article['image'] ?>  " alt="image for article"> </div>
+
+
+                        <div class="article-subtext">
+
+                            <p> <?= $article['subtext']; ?> </p>
+                        </div>
+
+                        <button class="read_more"> Read more..</button>
+
+
+                        <div class="article_text">
+
+                            <p> <?= $article['content']; ?> </p>
+                        </div>
+
 
                         <div class="info_wrapper">
                             <p class="info_item">
-                                Written by:
+                                Written by: <br>
                                 <?= $article['author']; ?>
                             </p>
 
@@ -69,8 +88,8 @@ require __DIR__ . '/data.php'; ?>
                                 <?= $article['published_date']; ?>
                             </p>
 
-                            <button class="info_item">
-                                Likes:
+                            <button class="likes">
+                                Likes
                                 <?= $article['likes']; ?>
                                 </p>
                         </div>
@@ -86,15 +105,16 @@ require __DIR__ . '/data.php'; ?>
             </div>
 
 
-        </div>
-
-    </section>
 
 
-    </div>
+        </section>
+
+    </main>
+
+
+
 
     <footer class="footer">
-        <p> Footer</p>
 
     </footer>
 
